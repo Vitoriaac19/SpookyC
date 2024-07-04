c1bc6f0e58057be7a0209190ac53df76c3c152d9
Rui Sousa <rui.sousa@minderacodeacademy.com>
2024-07-04 12:31:36 +0100

    change winner ascii art and more comments

--- src/resources/Quiz.java
+++ src/resources/Quiz.java
@@ -5,9 +5,15 @@ import java.util.Map;
 
 public class Quiz {
 
+    /**
+     * Variable defined that is a map, that links strings (room names) to a list of questions
+     */
     private Map<String, List<Question>>
             subjects;
 
+    /**
+     * Getter for the map to have access to the questions
+     */
     public Map<String, List<Question>> getSubjects() {
         return subjects;
     }
