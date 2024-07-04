1c4a1584087aacedf47d464d74f85256600f25ae
Rui Sousa <rui.sousa@minderacodeacademy.com>
2024-07-04 11:50:18 +0100

    add some comments to classes and fix to don't get repeated keys

--- src/resources/Question.java
+++ src/resources/Question.java
@@ -3,18 +3,30 @@ package resources;
 import java.util.List;
 
 public class Question {
+    /**
+     * This class is used to manage the questions and their answers
+     */
     private String question;
     private List<String> answers;
     private int correctAnswer;
 
+    /**
+     * A string that holds the text of the question
+     */
     public String getQuestion() {
         return question;
     }
 
+    /**
+     * A list for the possible answers of the question
+     */
     public List<String> getAnswers() {
         return answers;
     }
 
+    /**
+     * An integer that holds the index of the correct answer
+     */
     public int getCorrectAnswerIndex() {
         return correctAnswer;
     }
--- src/resources/Quiz.java
+++ src/resources/Quiz.java
@@ -5,7 +5,8 @@ import java.util.Map;
 
 public class Quiz {
 
-    private Map<String, List<Question>> subjects;
+    private Map<String, List<Question>>
+            subjects;
 
     public Map<String, List<Question>> getSubjects() {
         return subjects;
